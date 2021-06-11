d69e0f73b9f37e6647ca7f9222c895c5a312762e
Haidy-saher <85006286+Haidy-saher@users.noreply.github.com>
2021-06-11 09:45:06 +0200

    comments added [LCD&LED.test.c]

--- LCD&LED_Test.c
+++ LCD&LED_Test.c
@@ -22,23 +22,24 @@ void udelay(int n)
 
 }
 void LCD_init(void) {
-    volatile unsigned long delay;
-    //portB
-    SYSCTL_RCGC2_R |= 0X00000002;
-    delay = SYSCTL_RCGC2_R;
-    GPIO_PORTB_AFSEL_R &= ~0xFF;
-    GPIO_PORTB_AMSEL_R &= ~0XFF;
-    GPIO_PORTB_PCTL_R &= ~0XFF;
-    GPIO_PORTB_DIR_R  |= 0XFF;
-    GPIO_PORTB_DEN_R  |= 0XFF;
-     //portA
-    SYSCTL_RCGC2_R |= 0X00000001;
-    delay = SYSCTL_RCGC2_R;
-    GPIO_PORTA_AFSEL_R &= ~0xE0;
-    GPIO_PORTA_AMSEL_R &= ~0XE0;
-    GPIO_PORTA_PCTL_R &= ~0XE0;
-    GPIO_PORTA_DIR_R |= 0XE0;
-    GPIO_PORTA_DEN_R |= 0XE0;
+     volatile unsigned long delay;
+    //portB//
+    SYSCTL_RCGC2_R |= 0X00000002;//port B clock enabled
+    delay = SYSCTL_RCGC2_R; //give time for clock start
+    GPIO_PORTB_AFSEL_R &= ~0xFF; //UART deactivation 
+    GPIO_PORTB_AMSEL_R &= ~0XFF; //analog disabled
+    GPIO_PORTB_PCTL_R &= ~0XFF; //used as i/o port
+    GPIO_PORTB_DIR_R  |= 0XFF; //portB pins used as input 
+    GPIO_PORTB_DEN_R  |= 0XFF; //Port B is digital enbled 
+
+      //portA//
+    SYSCTL_RCGC2_R |= 0X00000001; //Port A clock enabled
+    delay = SYSCTL_RCGC2_R; //give time for clock start
+    GPIO_PORTA_AFSEL_R &= ~0xE0; //UART deactivated,PA 6,7,8
+    GPIO_PORTA_AMSEL_R &= ~0XE0; //analog disabled, PA6,PA7,PA8
+    GPIO_PORTA_PCTL_R &= ~0XE0;// PA6,PA7,PA8 used as I/O
+    GPIO_PORTA_DIR_R |= 0XE0; //pA6,PA7,PA8 used as input  
+    GPIO_PORTA_DEN_R |= 0XE0; //pA6,PA7,PA8 is digital enabled
 }
 void LCD_command (unsigned char cmd) {
   LCD_RS = 0x00;
@@ -61,14 +62,14 @@ void initialize_portF(void){
 SYSCTL_RCGCGPIO_R|= 0x20;
 //delay = 1; //dummy var
 //while((SYSCTL_PRGPIO_R & 0x20) == 0);
-GPIO_PORTF_LOCK_R = 0x4C4F434B;
-GPIO_PORTF_CR_R =0x1F;
-GPIO_PORTF_PCTL_R =0;
-GPIO_PORTF_AFSEL_R = 0;
-GPIO_PORTF_AMSEL_R = 0 ;
-GPIO_PORTF_DEN_R = 0x1F;
-GPIO_PORTF_DIR_R = 0x0E;
-GPIO_PORTF_PUR_R = 0x11;
+GPIO_PORTF_LOCK_R = 0x4C4F434B; //key to unlock Port F
+GPIO_PORTF_CR_R =0x1F; // unlock PF0,PF1,PF2,PF3,PF4
+GPIO_PORTF_PCTL_R =0;//set as I/O port
+GPIO_PORTF_AFSEL_R = 0;//alternate functions disabled
+GPIO_PORTF_AMSEL_R = 0 ;//analog input disabled
+GPIO_PORTF_DEN_R = 0x1F;//PF0,PF1,PF2,PF3,PF4 digital enabled
+GPIO_PORTF_DIR_R = 0x0E;//PF1,PF2,PF3 used as input registers
+GPIO_PORTF_PUR_R = 0x11;//PF0,PF4 internal pull up resistor activation
 }
 void Display_num(void){
     LCD_command(0X30);
