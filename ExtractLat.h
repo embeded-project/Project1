771ab0776a4d56cf3d45aceb845afb000f814585
norhan abdelhafeez <norhanabdelhafeez4@gmail.com>
2021-06-13 23:15:15 +0200

    lcd function

--- ExtractLat.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-double ExtractLat(char* lat) {
-	double dlat = atof(lat);
-	double dlat2 = dlat / 100;
-	int dint = (int)dlat2;
-	double dpoint = (dlat2 - dint) * 100;
-	return dint + (dpoint / 60);
-}
--- ExtractLon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-double ExtractLon(char* lon) {
-	double dlon = atof(lon);
-	double dlon2 = dlon / 100;
-	int dint = (int)dlon2;
-	double dpoint = (dlon2 - dint) * 100;
-	return dint + (dpoint / 60);
-}
